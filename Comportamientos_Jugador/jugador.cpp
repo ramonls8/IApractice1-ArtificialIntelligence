d516310d28f5a314cc28b31357de84cc7668b0b8
ramonls8 <ramonls2002@gmail.com>
2023-03-16 20:18:45 +0100

    2

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -37,7 +37,7 @@ Action ComportamientoJugador::think(Sensores sensores){
 
 
 
-			// prueba de github, comentario
+			// prueba de github, comentario 
 
 	// Determinar el efecto de la ultima accion enviada
 	return accion;
