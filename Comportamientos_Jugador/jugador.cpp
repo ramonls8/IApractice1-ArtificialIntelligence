52b2e2d196497d912883469e746197eedff4d3f1
ramonls8 <ramonls2002@gmail.com>
2023-03-19 20:04:57 +0100

    1

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -2,17 +2,44 @@
 #include <iostream>
 using namespace std;
 
-
+//////////////////////////////////////////////////////////
+// Funciones
+//////////////////////////////////////////////////////////
 
 // Guarda la vista actual en el mapa
-void SaveView(const vector<unsigned char> &view, const state &st, vector<vector<unsigned char>> &map){
+void SaveView(vector<unsigned char> &view, const state &st, vector<vector<unsigned char>> &map){
 	map[st.fil][st.col] = view[0];
 }
 
+// Rellena todos los valores de una matriz con el carácter c
+void ResetCharMatrix(vector<vector<unsigned char>> &matrix, char c = '?'){
+	int rows = matrix.size();
+
+	for (int i=0; i<rows; i++){
+		int cols = matrix[i].size();
+
+		for (int j=0; j<cols; j++)
+			matrix[i][j] = c;
+	}
+}
+
+//////////////////////////////////////////////////////////
+// Métodos privados
 //////////////////////////////////////////////////////////
 
+bool ComportamientoJugador::ResetState(unsigned int size){
+	// La fila y columna serán las últimas para poder rellenar el mapa
+    // auxiliar sin salir de él en caso de no poder ubicarnos
+	st.fil = st.col = size-1;
+    st.brujula = norte;
+}
+
+bool ComportamientoJugador::SensorIsWorking(const Sensores &sensores){
+	return sensores.posF != -1;
+}
+
 void ComportamientoJugador::ShowInfo(const Sensores &sensores){
-	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC << " ";
+	cout << "Fil " << sensores.posF << ", Col " << sensores.posC << ", ";
 	switch(sensores.sentido){
 		case 0: cout << "Norte" << endl;	break;
 		case 1: cout << "Noreste" << endl;	break;
@@ -23,7 +50,7 @@ void ComportamientoJugador::ShowInfo(const Sensores &sensores){
 		case 6: cout << "Oeste" << endl;	break;
 		case 7: cout << "Noroeste" << endl;	break;
 	}
-	cout << "Terreno: ";
+	cout << "Terreno:    ";
 	for (int i=0; i<sensores.terreno.size(); i++)
 		cout << sensores.terreno[i];
 	cout << endl;
@@ -33,27 +60,21 @@ void ComportamientoJugador::ShowInfo(const Sensores &sensores){
 		cout << sensores.superficie[i];
 	cout << endl;
 
-	cout << "Colisión: " << sensores.colision << endl;
-	cout << "Reset: " << sensores.reset << endl;
-	cout << "Vida: " << sensores.vida << endl;
+	cout << "Colisión:   " << sensores.colision << endl;
+	cout << "Reset:      " << sensores.reset << endl;
+	cout << "Vida:       " << sensores.vida << endl;
 	cout << endl;
 }
 
-
-
-
-
-
-//////////////////////////////////////////////////////////
-
-Action ComportamientoJugador::think(Sensores sensores){
-
-	Action accion = actIDLE;
-
-	// Actualizar con la última acción
-	switch (last_action){
-		case actFORWARD:	// Da un paso si no hay colisión
-			if (!sensores.colision){
+void ComportamientoJugador::UpdateState(const Sensores &sensores){
+	// Sabemos la posición gracias a los sensores
+	if (SensorIsWorking(sensores)){
+		// Comprobamos si acabamos de llegar a una casilla de posicionamiento tras estar
+		// desubicados, donde pasaremos los datos que hemos descubierto en
+		// el mapa auxiliar al mapa resultado
+		if (wellLocated = false){
+			// Actualizamos nuestra posición en mapa auxiliar
+			if (last_action == actFORWARD)
 				switch (st.brujula){
 					case norte:		st.fil--; 			break;
 					case noreste:	st.fil--; st.col++;	break;
@@ -64,43 +85,98 @@ Action ComportamientoJugador::think(Sensores sensores){
 					case oeste:		st.col--;			break;
 					case noroeste:	st.fil--; st.col--;	break;
 				}
-			}
-			break;
+			// Pasamos el mapa auxiliar con nuestra posición en este al mapa resultado
+			// con la nueva posición
+
+			// corregir: TransferMap(mapaAuxiliar, st.fil, st.col, mapaResultado, sensores.posF, sensores.posC)
+		}
 		
-		case actTURN_SL:
-			st.brujula =
-				static_cast<Orientacion>( (st.brujula+7) % 8 );
-			break;
-		case actTURN_SR:
-			st.brujula =
-				static_cast<Orientacion>( (st.brujula+1) % 8 );
-			break;
-		case actTURN_BL:
-			st.brujula =
-				static_cast<Orientacion>( (st.brujula+5) % 8 );
-			break;
-		case actTURN_BR:
-			st.brujula =
-				static_cast<Orientacion>( (st.brujula+3) % 8 );
-			break;
+		// Actualizamos el estado
+		st.fil = sensores.posF;
+		st.col = sensores.posC;
+		st.brujula = sensores.sentido;
+		wellLocated = true;
 	}
+	
+	// Hay reset y no sabemos la nueva ubicación
+	else if (sensores.reset){
+		wellLocated = false;
+
+		// Como no sabemos donde estamos, reiniciamos el mapa auxiliar y el estado
+		ResetCharMatrix(mapaAuxiliar);
+		ResetState(mapaResultado.size());
+	}
+
+	// No hay reset y no valen los sensores, por lo que tenemos en cuenta last_action
+	else {
+		switch (last_action){
+			case actFORWARD:	// Da un paso si no hay colisión
+				if (!sensores.colision){
+					switch (st.brujula){
+						case norte:		st.fil--; 			break;
+						case noreste:	st.fil--; st.col++;	break;
+						case este:		st.col++;			break;
+						case sureste:	st.fil++; st.col++;	break;
+						case sur:		st.fil++;			break;
+						case suroeste:	st.fil++; st.col--;	break;
+						case oeste:		st.col--;			break;
+						case noroeste:	st.fil--; st.col--;	break;
+					}
+				}
+				break;
+			
+			case actTURN_SL:
+				st.brujula =
+					static_cast<Orientacion>( (st.brujula+7) % 8 );
+				break;
+			case actTURN_SR:
+				st.brujula =
+					static_cast<Orientacion>( (st.brujula+1) % 8 );
+				break;
+			case actTURN_BL:
+				st.brujula =
+					static_cast<Orientacion>( (st.brujula+5) % 8 );
+				break;
+			case actTURN_BR:
+				st.brujula =
+					static_cast<Orientacion>( (st.brujula+3) % 8 );
+				break;
+		}
+	}
+
+	/*
+	// Si no conocía la posición y pisa una casilla G, a partir de ahora sí la conocerá
+	if (SensorIsWorking(sensores) && !wellLocated){
+		st.fil = sensores.posF;
+		st.col = sensores.posC;
+		st.brujula = sensores.sentido;
+		wellLocated = true;
+	}
+	*/
+}
+
+
+
+
+//////////////////////////////////////////////////////////
+// Métodos públicos
+//////////////////////////////////////////////////////////
+
+Action ComportamientoJugador::think(Sensores sensores){
+	// Crea la acción
+	Action accion = actIDLE;
+
+	// Actualizar estado
+	UpdateState(sensores);
 
 	// Info debug
 	ShowInfo(sensores);
 
-
-
-	// Si no conocía la posición y pisa una casilla G, a partir de ahora sí la conocerá
-	if (sensores.posF!=-1 and !bien_situado){
-		st.fil = sensores.posF;
-		st.col = sensores.posC;
-		st.brujula = sensores.sentido;
-		bien_situado = true;
-	}
-
-	// Guarda lo que ve en el mapa temporal o en el mapa resultado
-	if (bien_situado)
+	// Guarda la vista en el mapa
+	if (wellLocated)
 		SaveView(sensores.terreno, st, mapaResultado);
+	else
+		SaveView(sensores.terreno, st, mapaAuxiliar);
 
 	// Decidir la nueva acción
 	if ((sensores.terreno[2]=='T' or sensores.terreno[2]=='S') or
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -13,10 +13,16 @@ class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
-      // Constructor de la clase
-      st.fil = st.col = 99;
-      st.brujula = norte;
+      
+      ResetState(size);
       last_action = actIDLE;
+
+      // Crea el mapa auxiliar con suficientes casillas para no salirse
+      // nunca de él, aunque no sepamos nuestra posición en el mapa
+      unsigned int auxSize = size*2-1;
+      vector<unsigned char> aux(auxSize, '?');
+      for (unsigned int i=0; i<auxSize; i++)
+        mapaAuxiliar.push_back(aux);
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -29,10 +35,22 @@ class ComportamientoJugador : public Comportamiento{
     // Variables de estado
     state st;
     Action last_action;
-    bool bien_situado = true;
+    bool wellLocated = false;
+    vector<vector<unsigned char>> mapaAuxiliar;
+
+
+    // Devuelve el estado a los valores por defecto
+    bool ResetState(unsigned int size);
+
+    // Devuelve si el sensor funciona y puede conocer la posición
+    bool SensorIsWorking(const Sensores &sensores);
 
     // Informa de la posición actual
     void ShowInfo(const Sensores &sensores);
+
+    // Actualiza state tras la última acción
+    void UpdateState(const Sensores &sensores);
+
 
 };
 
